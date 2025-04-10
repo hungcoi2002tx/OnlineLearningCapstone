6889045dffc81af07fb01536e26ef0a6a01726bb
HunmNm <hackinghungtx@gmail.com>
2025-04-10 21:42:39 +0700

    up

--- AssignmentService/Share/Extentions/AutoMapperExtention.cs
+++ AssignmentService/Share/Extentions/AutoMapperExtention.cs
@@ -12,6 +12,28 @@ namespace Share.Extentions
             services.AddAutoMapper(assembly);
             #endregion
             services.AddAutoMapper(typeof(MapperConfig));
-        }   
+        }
+        
+    }
+
+    public class MapperConfig : AutoMapper.Profile
+    {
+        public MapperConfig()
+        {
+            var assembly = Assembly.GetAssembly(typeof(MapperConfig)); 
+            var models = assembly.ExportedTypes.Where(x => x.Namespace.Equals("Share.Model"));
+            var dtos = assembly.ExportedTypes.Where(x => x.Namespace.Equals("Share.DTO"));
+
+            foreach (var type in models)
+            {
+                CreateMap(type, type).ReverseMap();
+
+                var dtoType = dtos.FirstOrDefault(x => x.Name == type.Name + "Dto");
+                if(dtoType != null)
+                {
+                    CreateMap(type, dtoType).ReverseMap();
+                }
+            }
+        }
     }
 }
